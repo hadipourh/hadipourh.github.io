233f175270365a89426b7d8beeaa2d790d5c98de
hadipourh <hsn.hadipour@gmail.com>
2025-09-09 01:04:03 +0200

    Temporarily remove custom domain for testing

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,6 +11,6 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
     integrations: [react(), tailwind(), sitemap()],
-    site: 'https://hoseinhadipour.com',
+    site: 'https://hadipourh.github.io',
     base: '/',
 });
